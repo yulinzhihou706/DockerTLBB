0776aeaf043afa82318c684a4842e999887d11b1
Dark.Hades <dark.hades.1102@gmail.com>
2019-03-30 19:17:34 +0900

    Script

--- push.hub.docker.sh
+++ push.hub.docker.sh
@@ -1,9 +1,9 @@
 docker tag tlbb/game_server:centos tlbb/game_server:centos
 docker push tlbb/game_server
 
-docker tag tlbb/tlbbdb tlbb/tlbbdb
+docker tag tlbb/tlbbdb:latest tlbb/tlbbdb:latest
 docker push tlbb/tlbbdb
 
-docker tag tlbb/webdb tlbb/webdb
+docker tag tlbb/webdb:latest tlbb/webdb:latest
 docker push tlbb/webdb
 
